7c0b2b417b18a32d7a3daedd8de5070ac3984b99
victor carrillo <gatitto079@gmail.com>
2022-06-20 01:23:28 -0500

    first commit

---
+++ _next/static/NuTGM_PR-bLarAZcg22OY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/981-26f3a53118c8e0a6.js","static/chunks/pages/index-689622639229759c.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
